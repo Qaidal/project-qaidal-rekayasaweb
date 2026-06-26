5e799b612c07f0f2aa4b3810bec46adb8ae7ed44
mac <mac@macs-MacBook-Air-2.local>
2026-06-27 01:01:00 +0700

    Fix undefined variable news in HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Service;
+use App\Models\News;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -44,11 +45,21 @@ class HomeController extends Controller
 
     public function news()
     {
-        return view('pages.news');
+        // 1. Ambil semua data berita dari database Aiven, urutkan dari yang terbaru
+        $news = News::latest()->get(); 
+
+        // 2. Kirim variabel $news ke file blade
+        return view('pages.news', compact('news'));
     }
 
     public function newsDetail($id)
     {
-        return view('pages.news-detail');
+        // Masukkan sekalian logic detail berita agar tidak error saat diklik nanti
+        $newsItem = News::findOrFail($id);
+        
+        // Ambil berita lain untuk rekomendasi di halaman detail berita
+        $otherNews = News::where('id', '!=', $id)->latest()->take(3)->get();
+
+        return view('pages.news-detail', compact('newsItem', 'otherNews'));
     }
 }
